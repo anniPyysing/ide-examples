61bd9cfb82c155de6978fc988327b5e1e44ab943
Sri Harsha Vathsavayi <sriharsha1250@gmail.com>
2018-05-29 22:10:14 +0300

    added text related to Otaniemi

--- task1.py
+++ task1.py
@@ -7,10 +7,10 @@ from __future__ import print_function
 import re
 from collections import Counter
 
-s = "Turku is a city on the southwest coast of Finland at the mouth of the Aura River"
+string_to_check = 'Otaniemi is the home of the Aalto University engineering schools campus, thus "Otaniemi" is often used as a synonym for Aalto.'
 
 # find all words in the string
-#TODO: type following code "words = re.findall('[a-zA-Z_]+', s)" below, as you type you get support from PyCharm
+#TODO: type following code "words = re.findall('[a-zA-Z_]+', string_to_check)" below, as you type you get support from PyCharm
 
 # counts the number each time a word appears
 #TODO: type following code "word_counts = Counter(words)" below
