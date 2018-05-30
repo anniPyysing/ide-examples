0a4133064acb98694e08d201b2ac9f50b7e8a3ae
anniPyysing <anni.pyysing@gmail.com>
2018-05-30 14:53:03 +0300

    "correction to task 1"

--- task1.py
+++ task1.py
@@ -7,13 +7,13 @@ from __future__ import print_function
 import re
 from collections import Counter
 
-string_to_check = 'Otaniemi is the home of the Aalto University engineering schools campus, thus "Otaniemi" is often used as a synonym for Aalto.'
+string_to_check = 'Otaniemi is the home of the Aalto ...' \
+                  'University engineering schools campus, thus "Otaniemi" is often used as a synonym for Aalto.'
 
 # find all words in the string
-#TODO: type following code "words = re.findall('[a-zA-Z_]+', string_to_check)" below, as you type you get support from PyCharm
+words = re.findall('[a-zA-Z_]+', string_to_check)
 
 # counts the number each time a word appears
-#TODO: type following code "word_counts = Counter(words)" below
-
+word_counts = Counter(words)
 
 print('words in given text are {}'.format(dict(word_counts)))
